72f9c316f09f5e032f2b142181f8f187b6ebd9eb
jkalamarz <jkalamarz@gmail.com>
2018-11-16 23:34:32 -0800

    Minor update

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -27,11 +27,18 @@ namespace radioapi.Controllers
             return dbContext.Program.Where(f => f.RadioId == id).OrderByDescending(f => f.Timestamp).Take(top).ToList();
         }
 
+        // GET api/1/search/strefa
+        [HttpGet("{id}/search/{filter}")]
+        public ActionResult<IEnumerable<Db.Program>> Search(int id, string filter)
+        {
+            return dbContext.Program.Where(f => f.RadioId == id).Where(p => p.Title.Contains(filter) || p.Author.Contains(filter)).OrderByDescending(f => f.Timestamp).Take(50).ToList();
+        }
+
         // GET api/1/dates
         [HttpGet("{id}/dates")]
         public ActionResult<IEnumerable<DateTime>> Dates(int id)
         {
-            return dbContext.Program.FromSql("select distinct DATE(timestamp) as Timestamp from program where radio_id={0};", id).Select(p => p.Timestamp).ToList();
+            return dbContext.Program.FromSql("select distinct DATE(timestamp) as Timestamp from program where radio_id={0}", id).Select(p => p.Timestamp).ToList();
         }
     }
 }
